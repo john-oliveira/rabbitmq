92e8a2efaf4c074420184e4befcaf6e569445d35
John, Oliveira <elton.pmp@gmail.com>
2022-02-13 14:14:05 -0300

    Initial Commit

---
+++ src/receive.ts
@@ -0,0 +1,43 @@
+import amqp, { Connection } from "amqplib";
+
+const user = 'admin';
+const pass = '123';
+var exchange = 'ex.direct';
+var exchangeType = 'direct';
+var queue = 'payment';
+var bindingKey = 'test'; //fanout exchange ignore binding key
+
+const receive = async () => {
+    const connection = await amqp.connect(`amqp://${user}:${pass}@localhost:5672`);
+    const channel = await connection.createChannel();
+    await channel.assertExchange(exchange, exchangeType, { durable: true });
+    await channel.assertQueue(queue, { durable: true });
+    await channel.bindQueue(queue, exchange, bindingKey);
+
+    /*we can use the prefetch method with the value of 1. 
+      This tells RabbitMQ not to give more than one message to a worker at a time. 
+      Or, in other words, don't dispatch a new message to a worker until it has processed and acknowledged the previous one. 
+      Instead, it will dispatch it to the next worker that is not still busy.*/
+    await channel.prefetch(1);
+
+    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+
+    channel.consume(queue, (msg) => {
+        console.log(" [x] %s: '%s'", msg!.fields.routingKey, msg!.content.toString());
+        sleep(100); //Fake process
+        channel.ack(msg!);
+        console.log(" [x] Done");
+    }, {
+        noAck: false //noAck means "no manual acknowledgements", so, automatic mode.
+    });
+}
+
+function sleep(milliseconds: number) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+        currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+}
+
+receive();
---
+++ src/send.ts
@@ -0,0 +1,27 @@
+import amqp, { Connection } from "amqplib";
+
+const user = 'admin';
+const pass = '123';
+var exchange = 'ex.direct';
+var exchangeType = 'direct';
+var routingKey = 'test';
+
+const send = async (msg: string) => {
+    const connection = await amqp.connect(`amqp://${user}:${pass}@localhost:5672`);
+    const channel = await connection.createChannel();
+    await channel.assertExchange(exchange, exchangeType, { durable: true });
+    channel.publish(exchange, routingKey, Buffer.from(msg), { persistent: true });
+    console.log(" [x] Sent %s", msg);
+    channel.close();
+    setTimeout(() => {
+        connection.close();
+    }, 500);
+}
+
+var number = 1;
+const timer = setInterval(()=>{
+    send(`Hello World! - ${number++}`);
+    if(number > 10000){
+        clearInterval(timer);
+    }
+}, 5)
